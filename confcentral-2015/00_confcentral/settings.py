79ef2ac2811b6e093a41ba3d8b337446625e6ac6
manibhushan <manibhushan.cs@gmail.com>
2015-05-02 13:59:49 +0530

    conf central auth user code

---
+++ confcentral-2015/00_confcentral/settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs Console or
+# Cloud Console.
+WEB_CLIENT_ID = '563251341746-8ne3b1jjg700lfaj0qsksk86uv9nfeob.apps.googleusercontent.com'
